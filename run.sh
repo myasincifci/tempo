861e3c4d1833b666209431c1fca70bbccf6579d6
Yasin Cluster <cifci@campus.tu-berlin.de>
2023-03-17 15:04:42 +0000

    modified run script

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 #$ -binding linear:4 # request 4 cpus (8 with Hyperthreading) (some recommend 4 per GPU)
-#$ -N tempo       # set consistent base name for output and error file (allows for easy deletion alias)
+#$ -N baseline       # set consistent base name for output and error file (allows for easy deletion alias)
 #$ -q all.q    # don't fill the qlogin queue (can some add why and when to use?)
 #$ -cwd        # change working directory (to current)
 #$ -V          # provide environment variables
 #$ -t 1-5      # start 100 instances: from 1 to 100
 #$ -l cuda=1   # request one GPU
 
-../env/bin/python tools/train_net_new.py --basline True --eval finetune
+array=("x" "1" "5" "10" "15" "20")
+../env/bin/python tools/linear_eval.py \
+    --path model_zoo/baseline.pth \
+    --runs 100 \
+    --name linear_eval/baseline \
+    --samples_pc "${array[$SGE_TASK_ID]}"
